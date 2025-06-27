f661f0161c8841c4e2cd6555da9eef60689652e0
Vignesh-3093 <kva3093@gmail.com>
2025-06-27 17:27:34 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Home } from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
-import { Toaster } from "./components/UI/toaster";
+
 function App() {
   return (
     <>
-      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
